a85dcf3474b895cde22a13d8c970cef115fb513a
Rodrigo Gomes Gonçalves <rodrigogomesgoncalves5367@gmail.com>
2022-08-29 09:37:18 -0300

    Estruturação de Pastas Completa

---
+++ site/src/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Teste React JS
+          
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
