6fd55165d67a141a9bff007417d440b5518c16ce
Cauã Mendes <cauamgomes6@gmail.com>
2022-09-20 16:28:04 -0300

    Selecionar imagem e Algumas informaçoes ja add

--- site/src/Pages/Admin/CadastroProduto/index.js
+++ site/src/Pages/Admin/CadastroProduto/index.js
@@ -3,7 +3,7 @@ import './index.scss';
 import { Link } from 'react-router-dom';
 import Cabecalho04 from '../../../Components/Cabeçalho04';
 import CadProdLogo from '../../../assets/images/Cad-Prodfase02.svg'
-
+import SalvarImgIcon from '../../../assets/images/Salvar-Imagem.svg';
 
 export default function Cadastro(){
     return(
@@ -22,27 +22,59 @@ export default function Cadastro(){
                 <div className='cont-infos-cad'>
                     <div className='cont-infocad-001'>
                         <div className='contfilha1-infocad-001'>
-                            <h2>
+                            <h2 className='text1-infocad001'>
                                 Nome
-                            </h2>
-                            <input className='input-infocad001' />
-                            <h2>
+                            </h2> 
+                            <input className='input1-infocad001' />
+                            <h2 className='text2-infocad001'>
                                 Preço
                             </h2>
                             <input className='input2-infocad001'/>
-                            <h2>
+                            <h2 className='text2-infocad001'>
                                 Fabricante
                             </h2>
                             <input className='input3-infocad001'/>
-                            <h2>
+                            <h2 className='text2-infocad001'>
                                 Quantia em Estoque
                             </h2>
                             <input className='input4-infocad001'/>
                         </div>
                         <div className='contfilha2-infocad-001'>
+                            <h1 className='titulo-contfilha2-infocad001'>
+                                Imagens
+                            </h1>
+                            <div className='contneta1-infocad001'>
+                                <label for='arquivo1' className='selecionar-img1'>
+                                    <h1 className='text-contneta1'>
+                                        Selecionar Imagem 1
+                                    </h1>
+                                    <img src={SalvarImgIcon} />
+                                    <input name='arquivo1' id='arquivo1' type='file' className='input-img1' />
+                                </label>
+                                <label for='arquivo2' className='selecionar-img2'>
+                                    <h1 className='text-contneta1'>
+                                        Selecionar Imagem 2
+                                    </h1>
 
+                                    <img src={SalvarImgIcon} />
+                                    <input name='arquivo2' id='arquivo2' type='file' className='input-img1' />
+                                </label>
+                            </div>
+                            <label for='arquivo3' className='selecionar-img3'>
+                                    <h1 className='text-contneta1'>
+                                        Selecionar Imagem 3
+                                    </h1>
+
+                                    <img src={SalvarImgIcon} />
+                                    <input name='arquivo3' id='arquivo3' type='file' className='input-img1' />
+                                </label>
                         </div>
                     </div>    
+
+
+
+
+
                     <div className='espaço-cadastro'>
                         <label>Nome</label>
                         <input type='text' name='nome' className=''/>
@@ -125,17 +157,6 @@ export default function Cadastro(){
 
                     </div>
 
-                </div>
-
-                <div className='imagens'>
-                    <label>Imagem 1</label>
-                    <input type='file' />
-
-                    <label>Imagem 2</label>
-                    <input type='file' />
-
-                    <label>Imagem 3</label>
-                    <input type='file' />
                 </div>
 
                 <div className='btn'>
