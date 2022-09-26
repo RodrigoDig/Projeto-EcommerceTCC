f34f8f01e0ad25035f518edef299655f35dfa360
João Pedro <jopedrogcarli@gmail.com>
2022-09-26 09:49:52 -0300

    Terminando LOGIN I

--- site/src/Pages/User/LoginI/index.js
+++ site/src/Pages/User/LoginI/index.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import './index.scss';
+import '../../../assets/images/login-I.png'
+import Cabecalho03 from '../../../Components/Cabeçalho03';
 
 
 export default function Login(){
     return(
         <main>
-            <section className='fundo'>
+            <Cabecalho03 />
+            <div className='fundo'>Construa seu futuro! 
+                                Atravéz da Tecnologia, Junte-se a ByteSpeed</div>
 
-                <label className='UL'>USER LOGIN</label>
+                <section className='CARD'>
+
+                    <label className='UL'>USER LOGIN</label>
 
                     <div className='infos'>
 
-                        <input type='text' name='Login'/>
-
-                        <input type='text' name='Login'/>
+                             <input type='text' name='Login'/>
+                        <label></label>
+                            <input type='text' name='Login'/>
+                            <label className='LABEL'>ESQUECEU SUA SENHA?</label>
+                             <input type='text' name='Login'/>
 
                     </div>
 
 
                     <button className='btn' >Login</button>
+                    
+                    <label className='LABEL2'> Não possui uma conta? Acesse aqui e cadastre-se</label>
+                   
 
 
-
-            </section>
+                </section>
         </main>
     )
 }
