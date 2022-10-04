666455cbc4e6589e133feb37a91f2e03daa5e6bd
Rodrigo <rodrigogomesgoncalves5367@gmail.com>
2022-10-04 09:40:59 -0300

    Tela de consulta com api funcionando(Falta css)

--- site/src/Pages/Admin/ConsultarEstoque/index.js
+++ site/src/Pages/Admin/ConsultarEstoque/index.js
@@ -10,11 +10,17 @@ import Deletar from '../../../assets/images/lixeira-de-reciclagem.png';
 
 import { listarTodosProdutos, buscarProdutoNome } from '../../../Api/cadProdutoApi';
 
-export default function ConsEstoque(){
+export default function ConsEstoque() {
 
     const [produto, setProduto] = useState([]);
+    const [filtro, setFiltro] = useState('');
 
-    async function carregarTodosProdutos(){
+    async function filtrar(){
+        const x = await buscarProdutoNome(filtro);
+        setProduto(x);
+    } 
+
+    async function carregarTodosProdutos() {
         const r = await listarTodosProdutos();
         setProduto(r);
     }
@@ -31,22 +37,22 @@ export default function ConsEstoque(){
             <section className='cont-001-estoque'>
                 <div className='cont-titulo-busca'>
                     <div className='cont-titulo-estoque'>
-                        <img src={EstoqueLogo} className='icon-estoque'/>
+                        <img src={EstoqueLogo} className='icon-estoque' />
                         <h1 className='titulo-estoque'>
                             Consultar Estoque
                         </h1>
-                    </div> 
+                    </div>
                     <div className='cont-busca-estoque'>
-                        <input className='input-pesquisa-estoque' placeholder='Buscar por ID'/>
-                        <button className='botao-pesquisa-estoque'>
+                        <input className='input-pesquisa-estoque' placeholder='Buscar por nome' value={filtro} onChange={e => setFiltro(e.target.value)}/>
+                        <button className='botao-pesquisa-estoque' onClick={filtrar}>
                             <img src={LupaIcon} height={21} width={21} />
                         </button>
                     </div>
                 </div>
                 <table>
                     <thead>
-                       <tr>
-                            <th>ID</th>
+                        <tr>
+                            <th>ID Departamento</th>
                             <th>Nome</th>
                             <th>Preço</th>
                             <th>Desconto</th>
@@ -56,25 +62,30 @@ export default function ConsEstoque(){
                             <th>Informações</th>
                             <th>Descrição</th>
                             <th>Garantia</th>
-                        </tr> 
+                        </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Cooler brabo</td>
-                            <td>520</td>
-                            <td>10</td>
-                            <td>5</td>
-                            <td>ryzen</td>
-                            <td>60</td>
-                            <td>coler brabo dms, muito tops</td>
-                            <td>coler movido a vapor do pc com bateria recarregavel</td>
-                            <td>01/01/2024</td>
-                            <td>
-                                <img src={Alterar}/>
-                                <img src={Deletar}/>
-                            </td>
-                        </tr>
+
+                        {produto.map(item =>
+                            <tr>
+                                <td>{item.idDepartamento}</td>
+                                <td>{item.nomeProduto}</td>
+                                <td>{item.valorProduto}</td>
+                                <td>{item.valorDesconto}</td>
+                                <td>{item.avaliacao}</td>
+                                <td>{item.fabricante}</td>
+                                <td>{item.estoque}</td>
+                                <td>{item.informações}</td>
+                                <td>{item.descricao}</td>
+                                <td>{item.garantia}</td>
+                                <td>
+                                    <img src={Alterar} />
+                                    <img src={Deletar} />
+                                </td>
+                            </tr>
+
+                        )}
+
                     </tbody>
                 </table>
             </section>
