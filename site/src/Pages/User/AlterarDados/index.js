1082f4c7c2c6530501333ef1c6d7052e344044b5
João Pedro <jopedrogcarli@gmail.com>
2022-10-17 09:38:06 -0300

    Fazendo Tela de ALTERAR DADOS DO USUÁRIO.

---
+++ site/src/Pages/User/AlterarDados/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './index.scss';
+import Cabecalho03 from '../../../Components/Cabeçalho01';
+import User from '../../../assets/images/User.svg';
+import Cadeado from '../../../assets/images/cadeadoAD.svg';
+
+
+
+export default function AlterarDados(){
+    return(
+        <main className='fundo'>
+            <Cabecalho03/>
+            <section className='fundo2'>
+                
+                <img src={User} className='logo-cad-user'/> 
+                <h1>Meus Dados</h1>
+                <p>Dados Básicos</p> <button>Alterar Senha</button>
+                <div className='infos'> 
+                    <input type="text" placeholder='Nome Completo  |'/>
+                    <input type="text" placeholder='Email  |'/> <img src={Cadeado} className='logo-cad-user'/> 
+                    <input type="text" placeholder='CPF |' />
+                    <input type="text" placeholder='RG  |'/>
+                    <input type="text" placeholder='Telefone  |'/>
+                    <br/>
+
+                    <div className='Sexualidade'>
+                        <p>SEXO</p>
+                        <input type="radio" className='inputR'/> <label>Masculino</label>
+                    </div>
+                    
+                    
+                    
+                
+                </div>
+            </section>
+        </main>
+    )
+}
