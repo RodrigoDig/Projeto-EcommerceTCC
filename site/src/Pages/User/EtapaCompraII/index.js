7e34234dff5cec263d936ee19014f289ab82b633
Arthur Miguel <arthurmiguel241006@gmail.com>
2022-10-27 09:02:13 -0300

    alterações etapaII (incompleta)

--- site/src/Pages/User/EtapaCompraII/index.js
+++ site/src/Pages/User/EtapaCompraII/index.js
@@ -4,14 +4,15 @@ import Cabeçalho05 from '../../../Components/Cabeçalho05'
 import { useNavigate } from 'react-router-dom';
 import Etapa from '../../../Components/etapas';
 import Tracos from '../../../assets/images/options.svg';
+import Cart from '../../../assets/images/cartaob.svg';
     
 
 export default function EtapaCompraII(){
 
     const navigate = useNavigate();
 
-    function irHm(){
-        navigate('/')
+    function voltar(){
+        navigate('/etapaI')
     }
 
     return(
@@ -40,20 +41,40 @@ export default function EtapaCompraII(){
                                             <div className='cont-zbx'>
                                                 <button className='cdp'>chave pix</button>
                                                 <button className='cdc'>
-                                                    <img className='idz'/>
+                                                    <img className='idz' src={Cart} />
                                                     cartão de crédito
                                                 </button>
                                                 <h1 className='ncx'> Utilizar cartão cadastrado?</h1>
                                             </div>
 
                                             <div className='const-adl'>
-                                                <h1 className='ndx'>Cartão de crédito</h1>
-
-                                                <h1 className='mvb'>À vista com até 10% de desconto* ou tudo em até 10x sem juros!</h1>
+                                                <h1 className='ndx'>Cartão de crédito <h1 className='afs'> À vista com até 10% de desconto* ou tudo em até 10x sem juros! </h1> </h1>
 
                                                 <h1 className='pdt'>A bytespeed! aceita as bandeiras de cartão VISA, MasterCard, ELO, HiperCard, American Express e Diners - Todos em até 10x sem juros ou com desconto em até 3x!</h1>
 
                                                 <h1 className='dbz'> *O desconto poderá ser concedido ou não até o limite de 10%, podendo ser menor ou zero, de acordo com o detalhado nas descrições do produto e só será aplicado às vendas diretas e entregues pelo KaBuM!, não se aplicando aos produtos de Marketplace.</h1>                                        
+                                            </div>
+
+                                            <div className='dga'>
+                                                <ul className='akd'>
+                                                    <li className='gsd'> Novo Cartão</li>
+
+                                                    <div className='info-1'>
+                                                        <input className='asp' type="text" placeholder="Titular do Cartão" />
+                                                        <input type="text" placeholder="Numéro do Cartão" />
+                                                    </div>
+
+                                                    <div className='info-2'>
+                                                        <input type="text" placeholder="Validade" />
+                                                        <input type="text" placeholder="Verificação CVV" />
+                                                        <input type="text" placeholder="Cpf do Titular"  />
+                                                    </div>
+
+                                                    <div className='info-3'>
+                                                        <input type="text" placeholder="Nascimento" />
+                                                        <input type="text" placeholder="Forma de pagamento" />
+                                                    </div>
+                                                </ul>
                                             </div>
 
 
@@ -62,7 +83,7 @@ export default function EtapaCompraII(){
                         </div>
 
                                             <div className='cont-zxd'>
-                                                <button className='fjh1'> Voltar ao inicio</button>
+                                                <button onClick={voltar} className='fjh1'> Voltar</button>
                                             </div>
                 </div>
 
