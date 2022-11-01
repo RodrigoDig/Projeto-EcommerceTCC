c547a89a2fe8e9b6870b1346d16f7dd3b284126b
Arthur Miguel <arthurmiguel241006@gmail.com>
2022-11-01 09:33:57 -0300

    componente novo (Finalizado)

---
+++ site/src/Components/Cabeçalho06/index.js
@@ -0,0 +1,33 @@
+import './index.scss';
+import { Link } from 'react-router-dom';
+import Logo from '../Logo';
+import LupaIcon from '../../assets/images/Lupa-busca.svg';
+import Logouser from '../../../src/assets/images/login.png';
+import Carrin from '../../assets/images/cart.svg';
+
+export default function Cabecalho2(){
+    return(
+        <main>
+            <section className='cb-02'>
+                <div className='cont-lg'>
+                    <Logo className='lg-bt'/>
+                    <div className='alin'>
+                        <div className='cb-r'></div>
+                        <p className='p1'> Compre por departamento</p>
+                    </div>
+                </div>
+                <div className='cont-pqs'>
+                    <input className='input-pqs' placeholder='Busque aqui'/>
+                    <button className='botao-pqs'>
+                        <img src={LupaIcon} height={21} width={21} />
+                    </button>
+                </div>
+               
+                <div className='cont-lgn'>
+                    <img className='ibc' src={Carrin} />
+                    <img src={Logouser} />
+                </div>
+            </section>
+        </main>
+    )
+}
