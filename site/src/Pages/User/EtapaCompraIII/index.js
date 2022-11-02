d528433557d7a89c4a373f30043e6db5587d6497
caua <cauamgomes@gmail.com>
2022-11-02 14:38:08 -0300

    Etapa 3 html e css completo, aguardo api da etapa 1, 2..

--- site/src/Pages/User/EtapaCompraIII/index.js
+++ site/src/Pages/User/EtapaCompraIII/index.js
@@ -8,6 +8,7 @@ import User from '../../../assets/images/user-icon.svg';
 import Pegada from '../../../assets/images/pegada-icon.svg';
 import Correto from '../../../assets/images/correto-icon.svg';
 import Info from '../../../assets/images/info-pedido.svg';
+import Basket from '../../../assets/images/basket-icon.svg';
 
 export default function EtapaCompraIII(){
     return(
@@ -46,21 +47,72 @@ export default function EtapaCompraIII(){
                                     </p>
                                 </div>
                                 <div className='cont-dados-user'>
-
                                 </div>
 
                             </div>
                             <div className='cont-filha2-endereco'>
-
+                                <div className='cont-titulo-endereco'>
+                                    <h1 className='titulo-endereco'>
+                                        Endereço de entrega
+                                    </h1>
+                                    <p className='text-endereco'>
+                                        Informações que serão inseridas na nota fiscal do pedido.
+                                    </p>
+                                </div>
+                                <div className='cont-dados-user'>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className='cont-listaprod-filha1'>
+                        <div className='cont-titulo-infoped'>
+                            <img src={Basket} className='icon-infoped'/>
+                            <h1 className='titulo-infoped'>
+                                Lista de produtos
+                            </h1>
+                        </div>
+                        <div className='cont-infoprod-pedido'>
+                            <img className='img-produto'/>
+                            <p className='nome-produto'>
 
+                            </p>
+                            <div className='cont-qtd-produto'>
+
+                            </div>
+                            <div className='cont-valor-produto'>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className='cont-filha2-02'>
+                    <div className='cont-01-filha2'>
+                        <h1 className='titulo-resumo'>
+                            Resumo
+                        </h1>
+                        <p className='valor-pagar'>
+                            Valor total do(s) produto(s):
+                        </p>
+                        <div className='linhas'></div>
+                        <p className='text-frete'>
+                            Frete: 
+                        </p>
+                        <div className='linhas'></div>
+                        <div className='cont-subtotal'>
+                            <p className='titulo-subtotal'>
+                                Subtotal:
+                            </p>
+                        </div>
 
+                    </div>
+                    <div className='cont-02-filha2'>
+                         <button className='botao-finalizar'>
+                                Concluir
+                         </button>
+                         <button className='botao-voltar'>
+                                Voltar
+                         </button>
+                    </div>
                 </div>
             </section>
         </main>
