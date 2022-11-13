34d563b1b7cd6515a39c416d52b7a63bf587f3be
Arthur Miguel <arthurmiguel241006@gmail.com>
2022-11-13 18:58:36 -0300

    tela de atendimento 50%

--- site/src/Pages/User/Atendimentos/index.js
+++ site/src/Pages/User/Atendimentos/index.js
@@ -3,6 +3,9 @@ import './index.scss';
 import { useNavigate } from 'react-router-dom';
 import Cabecalho from '../../../Components/Cabeçalho06';
 import Interrogacao from '../../../assets/images/interrogacao.svg';
+import Correspondencia from '../../../assets/images/correspondencia-1.svg';
+import Reuniao from '../../../assets/images/reuniao-on-line-1.svg';
+import Telefone from '../../../assets/images/telefone-2.svg';
 
 export default function Atendimento(){
 
@@ -29,9 +32,29 @@ export default function Atendimento(){
                     </div>
 
                     <div className='align-4'>                   
-                        <div className='card-1'>a</div>
-                        <div className='card-2'>b</div>
-                        <div className='card-3'>c</div>
+                        <div className='card-1'>
+                            <h1 className='txt-3'>Email de contato</h1>
+                            <img className='img-2' src={Correspondencia} />
+                            <h1 className='txt-4'>Entre em contato conosco através</h1>
+                            <h1 className='txt-5'>do email</h1>
+                            <h1 className='txt-6'>bytespeedcontact@gmail.com</h1>
+                        </div>
+
+                        <div className='card-2'>
+                            <h1 className='txt-7'>Telefone SAC</h1>
+                            <img className='img-3' src={Telefone} />
+                            <h1 className='txt-9'>Segunda à sábado das 8:00 às</h1>
+                            <h1 className='txt-14'>20.</h1>
+                            <h1 className='txt-10'>Exceto domingo e feriados</h1>
+                            <h1 className='txt-11'>Número: (19) 2114.4444</h1>
+                        </div>
+
+                        <div className='card-3'>
+                            <h1 className='txt-12'>Chat Online</h1>
+                            <img className='img-4' src={Reuniao} />
+                            <h1 className='txt-13'>Atendimento ao vivo de Segunda à</h1>
+                            <h1 className='txt-15'>sábado das 8:00 às 20:00</h1>
+                        </div>
                     </div>
 
             </section>  
