a7df8977af7d854f9dcc48a1e0ba34cbe827895b
Rodrigo Gomes Gonçalves <rodrigogomesgoncalves5367@gmail.com>
2022-11-16 23:39:16 -0300

    Alterar  usuario com api's completas, falta apenas linkar

--- api/src/controller/cadastroUsuarioController.js
+++ api/src/controller/cadastroUsuarioController.js
@@ -1,5 +1,5 @@
 import { validacaoUsuario } from "../services/cadastroUsuarioValidacao.js";
-import { cadUser } from "../repository/cadastroUserRepository.js";
+import { cadUser, alterarUsuario, listarUsuarios, buscarNome, buscarId } from "../repository/cadastroUserRepository.js";
 import { Router } from 'express';
 
 const server = Router();
@@ -16,6 +16,71 @@ server.post('/cadastro/user', async (req, resp)=> {
         return resp.status(400).send({
             erro: err.message
         });
+    }
+})
+
+server.put('/cadastro/user/:id', async(req, resp) =>{
+    try{
+        const {id} = req.params;
+        const usuario = req.body;
+        
+        await validacaoUsuario(usuario);
+        const resposta = await alterarUsuario(id, usuario);
+        if(resposta != 1)
+            throw new Error('Usuário não pode ser alterado!')
+        else
+            resp.status(204).send();
+    }
+    catch(err){
+        resp.status(400).send({
+            erro:err.message
+        }) 
+    }
+})
+
+server.get('/usuario', async (req, resp) =>{
+    try{
+        const usuario = await listarUsuarios();
+        resp.send(usuario);
+    }
+    catch(err){
+        resp.status(400).send({
+            erro:"Ops, algo não está funcionando corretamente!!"
+        })
+    }
+})
+
+server.get('/busca/usuario', async (req, resp) =>{
+    try{
+        const {nome} = req.query;
+        const usuario = await buscarNome(nome);
+
+        if(usuario.length == 0)
+            resp.status(404).send([])
+        else
+            resp.send(usuario);
+    }
+    catch(err){
+        resp.status(400).send({
+            erro:"Ops, algo não está funcionando corretamente!!"
+        })
+    }
+})
+
+server.get('/consulta/usuario/:id', async (req, resp) =>{
+    try{
+        const {id} = req.params;
+        const usuario = await buscarId(id);
+
+        if(!usuario)
+            resp.status(404).send([])
+        else
+            resp.send(usuario);
+    }
+    catch(err){
+        resp.status(400).send({
+            erro:"Ops, algo não está funcionando corretamente!!"
+        })
     }
 })
 
--- api/src/repository/cadastroUserRepository.js
+++ api/src/repository/cadastroUserRepository.js
@@ -1,6 +1,6 @@
 import { con } from "./connection.js";
 
-export async function cadUser(usuario){
+export async function cadUser(usuario) {
     const linhas = `
         INSERT INTO TB_USUARIO(
              NM_USUARIO,
@@ -24,6 +24,85 @@ export async function cadUser(usuario){
         usuario.celular,
         usuario.senha
     ])
-    
+
     return resp.insertId;
+}
+
+export async function alterarUsuario(id, usuario) {
+    const comando = `
+        UPDATE TB_USUARIO
+           SET  NM_USUARIO      =?,     
+                NM_SOBRENOME    =?,
+                DS_CPF          =?,
+                DT_NASCIMENTO   =?,
+                DS_GENERO       =?,
+                DS_EMAIL        =?,
+                DS_CELULAR      =?,
+                DS_SENHA        =?
+        WHERE ID_USUARIO = ?`;
+    const [resposta] = await con.query(comando, [
+        usuario.nome,
+        usuario.sobrenome,
+        usuario.cpf,
+        usuario.nascimento,
+        usuario.genero,
+        usuario.email,
+        usuario.celular,
+        usuario.senha,
+        id]);
+    return resposta.affectedRows;
+}
+
+export async function listarUsuarios() {
+    const comando = `
+        SELECT ID_USUARIO   id,
+            NM_USUARIO      usuario,     
+            NM_SOBRENOME    sobrenome,
+            DS_CPF          cpf,
+            DT_NASCIMENTO   nascimento,
+            DS_GENERO       genero,
+            DS_EMAIL        email,
+            DS_CELULAR      celular,
+            DS_SENHA        senha
+        FROM  TB_USUARIO`;
+
+    const [linhas] = await con.query(comando)
+    return linhas;
+}
+
+export async function buscarId(id) {
+    const comando = `
+        SELECT  ID_USUARIO      id,
+                NM_USUARIO      usuario,     
+                NM_SOBRENOME    sobrenome,
+                DS_CPF          cpf,
+                DT_NASCIMENTO   nascimento,
+                DS_GENERO       genero,
+                DS_EMAIL        email,
+                DS_CELULAR      celular,
+                DS_SENHA        senha
+            FROM  TB_USUARIO
+        WHERE ID_USUARIO = ?`;
+
+    const [linhas] = await con.query(comando, [id])
+    return linhas[0];
+}
+
+
+export async function buscarNome(nome) {
+    const comando = `
+        SELECT  ID_USUARIO      id,
+                NM_USUARIO      usuario,     
+                NM_SOBRENOME    sobrenome,
+                DS_CPF          cpf,
+                DT_NASCIMENTO   nascimento,
+                DS_GENERO       genero,
+                DS_EMAIL        email,
+                DS_CELULAR      celular,
+                DS_SENHA        senha
+          FROM  TB_USUARIO
+        WHERE NM_USUARIO LIKE ?`;
+
+    const [linhas] = await con.query(comando, [`%${nome}%`])
+    return linhas;
 }
